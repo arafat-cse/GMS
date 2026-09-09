064d01759ce41063208559a90e1e2f6dd292ac37
mdsamaul843 <mdsamaul843@gmail.com>
2026-09-10 00:02:53 +0600

    fix: update admin password in AdminSeeder to '12345'

--- app/backend-api/database/seeders/AdminSeeder.php
+++ app/backend-api/database/seeders/AdminSeeder.php
@@ -15,7 +15,7 @@ class AdminSeeder extends Seeder
             [
                 'first_name' => 'Super',
                 'last_name' => 'Admin',
-                'password' => Hash::make('password'),
+                'password' => Hash::make('12345'),
                 'role' => 'admin',
             ]
         );
